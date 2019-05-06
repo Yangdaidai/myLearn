37941d8f59ab9895555f0d5acc4caa3d8f155ea6
yangchang@lizihang.com <yangchang@lizihang.com>
2019-05-06 16:09:13 +0800

    SelectionSort

---
+++ src/main/java/com/young/mywork/common/collection/HashMapDemo.java
@@ -0,0 +1,27 @@
+package com.young.mywork.common.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class HashMapDemo {
+    public static void main(String[] args) {
+        Map<String, Integer> hashMap = new HashMap<>();
+        hashMap.put("yang chang",20);
+        hashMap.put("王小二",18);
+
+        hashMap.forEach((s, integer) -> System.out.println("name is : "+s+" age is : "+integer));
+
+        Set<String> stringSet = hashMap.keySet();
+        for (String s : stringSet) {
+            System.out.println("name : "+s+ " age : "+hashMap.get(s));
+        }
+
+        Set<Map.Entry<String, Integer>> entrySet = hashMap.entrySet();
+
+        for (Map.Entry entry : entrySet){
+            System.out.println("name : "+entry.getKey()+" age : "+entry.getValue());
+        }
+
+    }
+}
--- src/main/java/com/young/mywork/common/sort/BubbleSort.java
+++ src/main/java/com/young/mywork/common/sort/BubbleSort.java
@@ -1,5 +1,10 @@
 package com.young.mywork.common.sort;
 
+import org.apache.commons.lang3.ArrayUtils;
+
+/**
+ *
+ */
 public class BubbleSort {
     public static void main(String[] args) {
         double[] scores = {88.5, 99, 45, 90, 23.5, 10, 100, 78};
@@ -14,17 +19,17 @@ public class BubbleSort {
         }
     }
 
-    public static void bubbleSort(double[] array) {
-        if (array.length < 0) {
+    private static void bubbleSort(double[] array) {
+        if (ArrayUtils.isEmpty(array)) {
             return;
         }
 
-        for (int i = 0; i < array.length-1; i++) {//外层控制循环趟数 n个数需要n-1趟
-            for (int j = 0; j < array.length-1-i; j++) {//内层控制循环每一趟的次数
-                if (array[j]>array[j+1]){
-                    double temp=array[j+1];
-                    array[j+1]=array[j];
-                    array[j]=temp;
+        for (int i = 0; i < array.length - 1; i++) {//外层控制循环趟数 n个数需要n-1趟
+            for (int j = 0; j < array.length - 1 - i; j++) {//内层控制循环每一趟的次数
+                if (array[j] > array[j + 1]) {
+                    double temp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = temp;
                 }
             }
 
