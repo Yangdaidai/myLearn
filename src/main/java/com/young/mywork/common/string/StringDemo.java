e5d6abd36f674fc278409ef637ccc80cba3316f1
yangchang@lizihang.com <yangchang@lizihang.com>
2019-05-14 09:53:44 +0800

    string

--- src/main/java/com/young/mywork/common/string/StringDemo.java
+++ src/main/java/com/young/mywork/common/string/StringDemo.java
@@ -11,7 +11,7 @@ public class StringDemo {
     public static void main(String[] args) {
         String str = new String("xyz");
         System.out.println("str = " + str);
-
+        str.intern();
         //        上面的代码创建了两个对象，一个存放在堆中，一个存放在字符串常量池中。
         //        当然，需要我们注意的是，如果之前常量池中已经存在"xyz"这个字符串，那么，上面的语句就只会在堆中创建一个对象了。
 
@@ -33,5 +33,16 @@ public class StringDemo {
         // 存在该字符串，返回引用实例。
         // 若不存在，则实例化该字符串并放入池中。
 
+        String s3 = new String("1") + new String("1");
+        s3.intern();
+
+        String s4 = "11";
+
+        boolean b2 = s3 == s4;
+        System.out.println("b2 =  "+b2); //true
+        // s3.intern()，这一行代码时，是将s3中的“11”字符串放入字符串常量池中。
+        // s4 直接从字符串常量池中取值。
+
+        //Java1.7 字符串常量池在堆区
     }
 }
