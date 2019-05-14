48a449b0998a76d1015dec02dd97582ae9b589d5
yangchang@lizihang.com <yangchang@lizihang.com>
2019-05-14 20:53:47 +0800

    thread join

--- src/main/java/com/young/mywork/common/thread/ThreadTest4.java
+++ src/main/java/com/young/mywork/common/thread/ThreadTest4.java
@@ -22,5 +22,21 @@ public class ThreadTest4  implements Runnable {
             Thread thread = new Thread(new ThreadTest4());
             thread.start();
         }
+        System.out.println("main线程为："+Thread.currentThread().toString());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //使正在运行的Java线程转到阻塞状态，millisec参数设定的是线程的睡眠时间，
+        // 以毫秒为单位，sleep(long millisec)是静态方法，只能控制当前正在运行的线程。
+
+        //当Java线程在睡眠结束后，便会转为就绪（Runnable）状态。
+
+        //一个线程执行了sleep操作，如果这个线程获取到锁，那么sleep并不会让出锁。
+        //
+        //前面说了，一个线程在睡眠结束后，便会转为就绪状态，并不会立刻执行，需要等待CPU的调度，
+        // 那么sleep中指定的时间就是线程休眠的最短时间。
     }
 }
---
+++ src/main/java/com/young/mywork/common/thread/ThreadTest5.java
@@ -0,0 +1,43 @@
+package com.young.mywork.common.thread;
+
+import java.util.ArrayList;
+
+/**
+ * Author 10248
+ * Date 2019/5/14 19:57
+ * Description
+ */
+public class ThreadTest5 implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("新启线程：" + Thread.currentThread().toString());
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("main开始做事，main线程为："+Thread.currentThread().toString());
+
+        ArrayList<Thread> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Thread thread = new Thread(new ThreadTest5());
+            list.add(thread);
+            thread.start();
+        }
+        //不要在循环中使用try…catch…，应该把其放在最外层
+        try {
+            for (Thread thread : list) {
+
+                //父线程等待子线程结束之后再运行--join()
+                thread.join();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        //，main线程要等到每一个thread线程运行结束后，才会输出“main继续做事 ...”。
+        // 如果不加thread.join(),main线程和thread线程是并行的。
+        // 而加上thread.join(),程序就变成是顺序执行了。
+        System.out.println("main继续做事，main线程为："+Thread.currentThread().toString());
+    }
+}
