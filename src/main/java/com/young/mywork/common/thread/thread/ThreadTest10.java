2ce3501675e6a91f97d9b587d33a7bfd62f8d3af
yangchang@lizihang.com <yangchang@lizihang.com>
2019-05-21 18:02:17 +0800

    ThreadLocal

--- src/main/java/com/young/mywork/common/thread/thread/ThreadTest10.java
+++ src/main/java/com/young/mywork/common/thread/thread/ThreadTest10.java
@@ -21,7 +21,10 @@ public class ThreadTest10 implements Runnable {
         while (true) {
             //如果大于0,就进行卖票
             if (count > 0) {
-                synchronized (this) {  //加锁 会导致程序性能的下降
+                synchronized (this) {
+                    //同步代码块
+                    //加锁 会导致程序性能的下降
+                    //使用同步：在线程执行逻辑中，给读写共享变量的操作同时加锁处理，使得同一时刻只有一个线程可以操作访问共享数据。
                     if (count > 0) {
 
                         System.out.println(Thread.currentThread().getName() + "正在售票,剩余票数 :  " + count--);
@@ -34,11 +37,15 @@ public class ThreadTest10 implements Runnable {
         }
     }
 
+    //造成线程不安全的原因
     //    一个线程从开始执行到执行结束的过程，如果在执行过程中有一个对象的变量被其他线程锁修改，
     //    那么对于当前线程来说，就发生了线程安全问题。如果每次运行结果和单线程运行的结果不一致，那么就会产生线程安全问题。
 
-
     //运行多线程环境； 多线程中存在共享变量；多线程中操作共享变量，主要操作行为--写操作；
+
+    //    同步方法：在类中的方法上加synchronized修饰符； //同步方法中的锁是：this，即调用者本身；
+    //   静态同步方法: 在类中的静态方法上加synchronized //静态同步方法中的锁是：类名.class，即Class对象；
+    //    同步代码块：在需要同步的代码上写一个synchronized(Object obj){}代码块； //同步代码块中的锁是：任意对象；
 
     public static void main(String[] args) {
         ThreadTest10 ticket = new ThreadTest10();
