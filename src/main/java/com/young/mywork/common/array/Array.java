13507a736d7c2a04120531a030f6b8e45c3af2e1
yangchang@lizihang.com <yangchang@lizihang.com>
2019-05-29 10:57:23 +0800

    Random

--- src/main/java/com/young/mywork/common/array/Array.java
+++ src/main/java/com/young/mywork/common/array/Array.java
@@ -1,6 +1,7 @@
 package com.young.mywork.common.array;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class Array {
 
@@ -34,5 +35,17 @@ public class Array {
         int i = Arrays.binarySearch(score1, 98);
         //否则返回 -1 或 “-插入点”
         System.out.println("查找到99的位置 :  " + i);
+
+        //随机数
+        double random = Math.random();
+        System.out.println("random = " +(int) (random*10));
+
+        //种子数只是随机算法的起源数字，和生成的随机数的区间没有任何关系。
+        // 在没带参数构造函数生成的Random对象的种子缺省是当前系统时间的毫秒数。
+        Random random1 = new Random();
+
+        int anInt = random1.nextInt(100);// nextInt()中的100是随机数的上限,产生的随机数为0-100的整数,不包括100。
+        System.out.println("anInt = " + anInt);
+
     }
 }
