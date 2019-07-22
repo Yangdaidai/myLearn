080f733c6746c0cca46053bc37461d2eb639a3d5
young <1024826067@qq.com>
2019-07-22 22:11:15 +0800

    加锁

--- src/main/java/com/young/mywork/common/gof/singleton/Singleton2.java
+++ src/main/java/com/young/mywork/common/gof/singleton/Singleton2.java
@@ -4,12 +4,11 @@ import java.util.Objects;
 
 public class Singleton2 {
     //加锁版
-    private static Object object = new Object();
     private static Singleton2 singleton2;
 
     public static Singleton2 getInstance() {
         if (Objects.isNull(singleton2)) {
-            synchronized (object) {
+            synchronized (Singleton2.class) {
                 if (Objects.isNull(singleton2)) {
                     singleton2 = new Singleton2();
                 }
